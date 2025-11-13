86e5daff49da52bf93cdf06f7a203b2ea279c475
Nicolai Ommer <nicolai.ommer@gmail.com>
2025-11-13 16:52:41 +0100

    tests: Add e2e test for agent cards

--- test/e2e/agentgateway_e2e_test.go
+++ test/e2e/agentgateway_e2e_test.go
@@ -18,6 +18,7 @@ package e2e
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os/exec"
 	"time"
@@ -30,6 +31,8 @@ import (
 
 var _ = Describe("Agent Gateway", Ordered, func() {
 
+	var gatewayUrl string
+
 	// After each test, check for failures and collect logs, events,
 	// and pod descriptions for debugging.
 	AfterEach(func() {
@@ -40,16 +43,7 @@ var _ = Describe("Agent Gateway", Ordered, func() {
 		}
 	})
 
-	AfterEach(func() {
-		By("cleaning up test resources")
-		_, _ = utils.Run(exec.Command("kubectl", "delete", "agent",
-			"-f", "config/samples/runtime_v1alpha1_agent.yaml"))
-
-		_, _ = utils.Run(exec.Command("kubectl", "delete", "agent",
-			"-f", "config/samples/runtime_v1alpha1_agentgateway.yaml"))
-	})
-
-	It("should proxy A2A requests to agent", func() {
+	BeforeAll(func() {
 		// TODO: Once the agent gateway correctly updates itself upon agent changes, join agent and gateway
 		// into one file.
 		By("applying the agent")
@@ -73,9 +67,23 @@ var _ = Describe("Agent Gateway", Ordered, func() {
 
 		By("creating port-forward to the gateway")
 		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
+		DeferCleanup(cancel)
 		localPort, err := utils.PortForwardService(ctx, "default", "agent-gateway", 10000)
 		Expect(err).NotTo(HaveOccurred(), "Failed to create port-forward to agent gateway")
+
+		gatewayUrl = fmt.Sprintf("http://localhost:%d", localPort)
+	})
+
+	AfterAll(func() {
+		By("cleaning up test resources")
+		_, _ = utils.Run(exec.Command("kubectl", "delete", "agent",
+			"-f", "config/samples/runtime_v1alpha1_agent.yaml"))
+
+		_, _ = utils.Run(exec.Command("kubectl", "delete", "agent",
+			"-f", "config/samples/runtime_v1alpha1_agentgateway.yaml"))
+	})
+
+	It("should proxy A2A requests to agent", func() {
 
 		By("sending HTTP request to the gateway")
 		payload := map[string]interface{}{
@@ -97,8 +105,23 @@ var _ = Describe("Agent Gateway", Ordered, func() {
 				"metadata": map[string]interface{}{},
 			},
 		}
-		url := fmt.Sprintf("http://localhost:%d/mocked-agent-exposed-1/", localPort)
-		err = utils.PostRequest(url, payload)
+		body, err := utils.PostRequest(gatewayUrl+"/mocked-agent-exposed-1", payload)
 		Expect(err).NotTo(HaveOccurred(), "Failed to send POST request to agent gateway")
+
+		var responseMap map[string]interface{}
+		err = json.Unmarshal(body, &responseMap)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(responseMap["result"]).NotTo(BeNil())
+	})
+
+	It("should retrieve agent card with correct url", func() {
+		By("retrieving agent card from the gateway")
+		body, err := utils.GetRequest(gatewayUrl + "/mocked-agent-exposed-1/.well-known/agent-card.json")
+		Expect(err).NotTo(HaveOccurred(), "Failed to send GET request to agent-card endpoint")
+
+		var responseMap map[string]interface{}
+		err = json.Unmarshal(body, &responseMap)
+		Expect(err).NotTo(HaveOccurred(), "Failed to unmarshal agent card response")
+		Expect(responseMap["url"]).To(Equal(gatewayUrl + "/mocked-agent-exposed-1"))
 	})
 })
