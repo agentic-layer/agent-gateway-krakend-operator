28dc25a1b6df038f5ed9560f9fc8f49c53f8e585
Nicolai Ommer <nicolai.ommer@gmail.com>
2025-11-13 16:52:41 +0100

    tests: Add e2e test for agent cards

--- test/utils/http.go
+++ test/utils/http.go
@@ -10,22 +10,36 @@ import (
 )
 
 // PostRequest sends a POST request with a JSON payload to the specified URL
-func PostRequest(url string, payload any) error {
+func PostRequest(url string, payload any) ([]byte, error) {
 	payloadBytes, err := json.Marshal(payload)
 	if err != nil {
-		return fmt.Errorf("failed to marshal payload: %w", err)
+		return nil, fmt.Errorf("failed to marshal payload: %w", err)
 	}
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadBytes))
 	if err != nil {
-		return err
+		return nil, err
 	}
 	req.Header.Set("Content-Type", "application/json")
 
+	return request(req)
+}
+
+// GetRequest sends a GET request to the specified URL and returns the response body
+func GetRequest(url string) ([]byte, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return request(req)
+}
+
+func request(req *http.Request) ([]byte, error) {
 	client := &http.Client{Timeout: 10 * time.Second}
 	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("HTTP request failed: %w", err)
+		return nil, fmt.Errorf("HTTP request failed: %w", err)
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
@@ -34,10 +48,9 @@ func PostRequest(url string, payload any) error {
 		}
 	}(resp.Body)
 
+	bodyBytes, _ := io.ReadAll(resp.Body)
 	if resp.StatusCode != http.StatusOK {
-		bodyBytes, _ := io.ReadAll(resp.Body)
-		return fmt.Errorf("expected HTTP 200 status code, got: %d, body: %s", resp.StatusCode, string(bodyBytes))
+		return nil, fmt.Errorf("expected HTTP 200 status code, got: %d, body: %s", resp.StatusCode, string(bodyBytes))
 	}
-
-	return nil
+	return bodyBytes, nil
 }
